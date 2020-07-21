fb737c8c4916a3f2847efc22096b5c4fb8bd3e06
sergei-krylov <sergei.krylov@yahoo.com>
2020-07-21 15:06:02 +0300

    BOSA-20 Allow to upload jpg for non-prod env to fix default seeds and to not override it

--- lib/extends/decidim-core/app/uploaders/decidim/attachment_uploader.rb
+++ lib/extends/decidim-core/app/uploaders/decidim/attachment_uploader.rb
@@ -9,12 +9,16 @@ module AttachmentUploaderExtend
     protected
 
     def extension_white_list
+      return %w(jpg jpeg pdf) unless Rails.env.production? # To fix default seeds in non-prod env
+
       %w(pdf)
     end
 
     # CarrierWave automatically calls this method and validates the content
     # type fo the temp file to match against any of these options.
     def content_type_whitelist
+      return [%r{image\/}, %r{application\/pdf}] unless Rails.env.production? # To fix default seeds in non-prod env
+
       [
         %r{application\/pdf}
       ]
