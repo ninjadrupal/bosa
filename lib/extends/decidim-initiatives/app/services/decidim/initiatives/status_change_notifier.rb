339008afb6317bb1cba3e3c897eca8d046f06f6e
sergei-krylov <sergei.krylov@yahoo.com>
2020-07-29 12:34:14 +0300

    BOSA-72 Move recent changes made by OSP

--- lib/extends/decidim-initiatives/app/services/decidim/initiatives/status_change_notifier.rb
+++ lib/extends/decidim-initiatives/app/services/decidim/initiatives/status_change_notifier.rb
@@ -52,7 +52,7 @@ module StatusChangeNotifierExtend
         next unless initiative.author != committee_member.user
 
         Decidim::Initiatives::InitiativesMailer
-          .notify_state_change(initiative, committee_member.user, initiative.state)
+          .notify_state_change(initiative, committee_member.user)
           .deliver_later
       end
     end
@@ -61,14 +61,14 @@ module StatusChangeNotifierExtend
       initiative.followers.each do |follower|
         initiative.organization.admins.each do |user|
           Decidim::Initiatives::InitiativesMailer
-            .notify_state_change(initiative, user, initiative.state)
+            .notify_state_change(initiative, user)
             .deliver_later
         end
 
         next unless initiative.author != follower
 
         Decidim::Initiatives::InitiativesMailer
-          .notify_state_change(initiative, follower, initiative.state)
+          .notify_state_change(initiative, follower)
           .deliver_later
       end
     end
