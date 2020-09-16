4e3122453413ff04a5d7cd323e993a8653f1da8f
Adam Okoń <adam.okon@elegantcode.pl>
2020-09-16 09:28:19 +0200

    BOSA-97: move code from the app scope to the verifications_omniauth extension

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -141,5 +141,42 @@ Decidim.configure do |config|
   # }
 end
 
+require "decidim/verifications/omniauth/bosa_action_authorizer"
+
+Decidim::Verifications.unregister_workflow(:csv_census)
+Decidim::Verifications.unregister_workflow(:id_documents)
+Decidim::Verifications.unregister_workflow(:postal_letter)
+Decidim::Verifications.unregister_workflow(:sms)
+
+
+Decidim::Verifications.register_workflow(:dummy_authorization_handler) do |workflow|
+  workflow.form = "DummyAuthorizationHandler"
+  # workflow.action_authorizer = "DummyActionAuthorizer"
+end
+
+Decidim::Verifications.register_workflow(:another_dummy_authorization_handler) do |workflow|
+  workflow.form = "AnotherDummyAuthorizationHandler"
+end
+
+Decidim::Verifications.register_workflow(:csam) do |workflow|
+  workflow.engine = Decidim::Verifications::Omniauth::Engine
+  workflow.admin_engine = Decidim::Verifications::Omniauth::AdminEngine
+  workflow.action_authorizer = "Decidim::Verifications::Omniauth::BosaActionAuthorizer"
+  # workflow.form = "Decidim::Verifications::Omniauth::OmniauthAuthorizationForm"
+  workflow.omniauth_provider = :csam
+  workflow.minimum_age = 16
+  workflow.expires_in = 24.hours
+end
+
+Decidim::Verifications.register_workflow(:saml) do |workflow|
+  workflow.engine = Decidim::Verifications::Omniauth::Engine
+  workflow.admin_engine = Decidim::Verifications::Omniauth::AdminEngine
+  workflow.action_authorizer = "Decidim::Verifications::Omniauth::BosaActionAuthorizer"
+  # workflow.form = "Decidim::Verifications::Omniauth::OmniauthAuthorizationForm"
+  workflow.omniauth_provider = :saml
+  workflow.minimum_age = 16
+  workflow.expires_in = 24.hours
+end
+
 Rails.application.config.i18n.available_locales = Decidim.available_locales
 Rails.application.config.i18n.default_locale = Decidim.default_locale
