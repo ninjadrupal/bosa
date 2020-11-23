bfcca0e243e279053e82c1f99e9e4402d2698b75
sergei-krylov <sergei.krylov@yahoo.com>
2020-11-23 11:55:41 +0300

    BOSA-187 Change the main menu order

---
+++ lib/extends/decidim-core/lib/decidim/menu.rb
@@ -0,0 +1,32 @@
+# Clear menu items to reorder
+Decidim::MenuRegistry.destroy(:menu)
+
+Decidim.menu :menu do |menu|
+
+  menu.item I18n.t("menu.home", scope: "decidim"),
+            decidim.root_path,
+            position: 1,
+            active: :exclusive
+
+  menu.item I18n.t("menu.suggestions", scope: "decidim"),
+            decidim_suggestions.suggestions_path,
+            position: 2,
+            active: :inclusive
+
+  menu.item I18n.t("menu.assemblies", scope: "decidim"),
+            decidim_assemblies.assemblies_path,
+            position: 3,
+            if: Decidim::Assembly.where(organization: current_organization).published.any?,
+            active: :inclusive
+
+  menu.item I18n.t("menu.initiatives", scope: "decidim"),
+            decidim_initiatives.initiatives_path,
+            position: 4,
+            active: :inclusive
+
+  menu.item I18n.t("menu.help", scope: "decidim"),
+            decidim.pages_path,
+            position: 5,
+            active: :inclusive
+
+end
