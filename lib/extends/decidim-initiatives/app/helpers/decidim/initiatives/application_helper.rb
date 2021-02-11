0e46f75ec2da4bcd9353361a8812feafdd3d4d00
sergei-krylov <sergei.krylov@yahoo.com>
2021-02-11 10:07:38 +0300

    BOSAM-20 Update initiatives/suggestions filters order BOSAM-75 scopes: filters on initiatives/suggestions page in front office BOSAM-62 petitions: add the 3 answers as for suggestions, in the filter in front office

--- lib/extends/decidim-initiatives/app/helpers/decidim/initiatives/application_helper.rb
+++ lib/extends/decidim-initiatives/app/helpers/decidim/initiatives/application_helper.rb
@@ -8,37 +8,30 @@ module InitiativesApplicationHelperExtend
   included do
     include Decidim::CheckBoxesTreeHelper
 
-    def filter_sorts_values
+    def filter_states_values
       Decidim::CheckBoxesTreeHelper::TreeNode.new(
         Decidim::CheckBoxesTreeHelper::TreePoint.new("", t("decidim.initiatives.application_helper.filter_state_values.all")),
         [
-          Decidim::CheckBoxesTreeHelper::TreePoint.new("published", t("decidim.initiatives.application_helper.filter_state_values.published")),
-          Decidim::CheckBoxesTreeHelper::TreePoint.new("classified", t("decidim.initiatives.application_helper.filter_state_values.classified")),
-          Decidim::CheckBoxesTreeHelper::TreePoint.new("examinated", t("decidim.initiatives.application_helper.filter_state_values.examinated")),
-          Decidim::CheckBoxesTreeHelper::TreePoint.new("debatted", t("decidim.initiatives.application_helper.filter_state_values.debatted"))
+          Decidim::CheckBoxesTreeHelper::TreePoint.new("open", t("decidim.initiatives.application_helper.filter_state_values.open")),
+          Decidim::CheckBoxesTreeHelper::TreeNode.new(
+            Decidim::CheckBoxesTreeHelper::TreePoint.new("closed", t("decidim.initiatives.application_helper.filter_state_values.closed")),
+            [
+              Decidim::CheckBoxesTreeHelper::TreePoint.new("accepted", t("decidim.initiatives.application_helper.filter_state_values.accepted")),
+              Decidim::CheckBoxesTreeHelper::TreePoint.new("rejected", t("decidim.initiatives.application_helper.filter_state_values.rejected"))
+            ]
+          ),
+          Decidim::CheckBoxesTreeHelper::TreeNode.new(
+            Decidim::CheckBoxesTreeHelper::TreePoint.new("answered", t("decidim.initiatives.application_helper.filter_state_values.answered")),
+            [
+              Decidim::CheckBoxesTreeHelper::TreePoint.new("examinated", t("decidim.initiatives.application_helper.filter_state_values.examinated")),
+              Decidim::CheckBoxesTreeHelper::TreePoint.new("debatted", t("decidim.initiatives.application_helper.filter_state_values.debatted")),
+              Decidim::CheckBoxesTreeHelper::TreePoint.new("classified", t("decidim.initiatives.application_helper.filter_state_values.classified"))
+            ]
+          ),
         ]
       )
     end
 
-    def filter_scopes_values
-      Rails.cache.fetch(current_organization.scopes.cache_key) do
-        main_scopes = current_organization.scopes.top_level
-
-        scopes_values = main_scopes.includes(:scope_type, :children).flat_map do |scope|
-          Decidim::CheckBoxesTreeHelper::TreeNode.new(
-            Decidim::CheckBoxesTreeHelper::TreePoint.new(scope.id.to_s, translated_attribute(scope.name, current_organization)),
-            scope_children_to_tree(scope)
-          )
-        end
-
-        scopes_values.prepend(Decidim::CheckBoxesTreeHelper::TreePoint.new("global", t("decidim.scopes.global")))
-
-        Decidim::CheckBoxesTreeHelper::TreeNode.new(
-          Decidim::CheckBoxesTreeHelper::TreePoint.new("", t("decidim.initiatives.application_helper.filter_scope_values.all")),
-          scopes_values
-        )
-      end
-    end
   end
 end
 
