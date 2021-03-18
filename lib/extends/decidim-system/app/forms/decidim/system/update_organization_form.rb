896a105ce9adb55c8b72b823efeaffe52946696a
sergei-krylov <sergei.krylov@yahoo.com>
2021-03-18 15:22:15 +0300

    BOSA21Q1-91 Allow to configure multiple allowed regions to create/sign a petition/suggestion

--- lib/extends/decidim-system/app/forms/decidim/system/update_organization_form.rb
+++ lib/extends/decidim-system/app/forms/decidim/system/update_organization_form.rb
@@ -9,9 +9,9 @@ module UpdateOrganizationFormExtend
     jsonb_attribute :initiatives_settings, [
       [:allow_users_to_see_initiative_no_signature_option, Boolean],
       [:create_initiative_minimum_age, Integer],
-      [:create_initiative_allowed_region, String],
+      [:create_initiative_allowed_regions, String],
       [:sign_initiative_minimum_age, Integer],
-      [:sign_initiative_allowed_region, String]
+      [:sign_initiative_allowed_regions, String]
     ]
 
     def set_from
