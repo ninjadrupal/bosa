a2008c1a944a3db72fd5f291b8e16ad1ba9c1714
Adam Okoń <adam.okon@elegantcode.pl>
2021-04-15 13:09:28 +0200

    BOSA21Q1-123: make tweaks for displaying flash messages with html entities

---
+++ lib/extends/decidim-core/app/helpers/decidim/flash_messages_helper.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+module FlashMessagesHelperExtend
+  extend ActiveSupport::Concern
+
+  included do
+    private
+
+    # Private: Foundation alert box.
+    #
+    # Overrides the foundation alert box helper for adding accessibility tags.
+    #
+    # value - The flash message.
+    # alert_class - The foundation class of the alert message.
+    # closable - A boolean indicating whether the close icon is added.
+    #
+    # Returns a HTML string.
+    def alert_box(value, alert_class, closable)
+      options = {
+        class: "flash callout #{alert_class}",
+        role: "alert",
+        aria: { atomic: "true" }
+      }
+      options[:data] = { closable: "" } if closable
+      content_tag(:div, options) do
+        concat sanitize(value, tags: %w(strong em a), attributes: %w(href))
+        concat close_link if closable
+      end
+    end
+  end
+end
+
+FoundationRailsHelper::FlashHelper.send(:include, FlashMessagesHelperExtend)
