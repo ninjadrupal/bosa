980dbd3fef6bad30549c98e197ffc969c558acca
sergei-krylov <sergei.krylov@yahoo.com>
2021-05-28 16:21:23 +0300

    BOSA21Q1-239 [decidim-0.24.1] Upgrade initiatives attachments to newer code

--- lib/extends/decidim-initiatives/app/forms/decidim/initiatives/initiative_form.rb
+++ lib/extends/decidim-initiatives/app/forms/decidim/initiatives/initiative_form.rb
@@ -12,25 +12,25 @@ module InitiativeFormExtend
     private
 
 
-    def notify_missing_attachment_if_errored
-      return if attachment.blank? || attachment.file.blank?
-
-      errors.add(:attachment, :needs_to_be_reattached) if errors.any?
-    end
-
-    def trigger_attachment_errors
-      return if attachment.blank? || attachment.file.blank?
-      return if attachment.valid?
-
-      if attachment.errors.has_key?(:title)
-        errors.add(:attachment, :title)
-        errors.add(:attachment, :needs_to_be_reattached)
-      elsif errors.keys.reject {|k| k == :attachment}.blank?
-        attachment.errors.each {|error| errors.add(:attachment, error)}
-        attachment = Attachment.new(file: attachment.try(:file))
-        errors.add(:attachment, :file) if !attachment.save && attachment.errors.has_key?(:file)
-      end
-    end
+    # def notify_missing_attachment_if_errored
+    #   return if attachment.blank? || attachment.file.blank?
+    #
+    #   errors.add(:attachment, :needs_to_be_reattached) if errors.any?
+    # end
+    #
+    # def trigger_attachment_errors
+    #   return if attachment.blank? || attachment.file.blank?
+    #   return if attachment.valid?
+    #
+    #   if attachment.errors.has_key?(:title)
+    #     errors.add(:attachment, :title)
+    #     errors.add(:attachment, :needs_to_be_reattached)
+    #   elsif errors.keys.reject {|k| k == :attachment}.blank?
+    #     attachment.errors.each {|error| errors.add(:attachment, error)}
+    #     attachment = Attachment.new(file: attachment.try(:file))
+    #     errors.add(:attachment, :file) if !attachment.save && attachment.errors.has_key?(:file)
+    #   end
+    # end
   end
 end
 
