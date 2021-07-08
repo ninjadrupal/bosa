1020052bdefe231473e2fc6543a618e1080fff0d
sergei-krylov <sergei.krylov@yahoo.com>
2021-07-08 14:46:12 +0300

    BOSA21Q1-375 1.5 Application vulnerable to Cross-Site Request Forgery (CSRF) - handle a CSRF token is not present or is incorrect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  rescue_from ActionController::InvalidAuthenticityToken do |exception|
+    sign_out
+    flash[:alert] = t("actions.unauthorized", scope: "decidim.core")
+    redirect_to '/'
+  end
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,6 +8,8 @@
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
   config.timeout_in = 30.minutes
+
+  config.parent_controller = "ApplicationController"
 end
 
 require "omniauth/strategies/eid_saml"
