6492ccdeaf92b63a188085fa24276fa2321937f8
Thomas Collignon <thomas.collignon@belighted.com>
2022-04-28 11:48:51 +0200

    BOSA21Q1-647 Fix perf issue - decryption of organization settings should be cached

--- lib/extends/decidim-core/app/models/decidim/organization.rb
+++ lib/extends/decidim-core/app/models/decidim/organization.rb
@@ -42,7 +42,7 @@ module OrganizationExtend
       return true if !user || user.admin?
       allowed_regions = initiatives_settings_allowed_regions(action)
       return true if allowed_regions.blank?
-      region_codes = Decidim::Organization::INITIATIVES_SETTINGS_ALLOWED_REGIONS.slice(*allowed_regions).values.pluck(:municipalities).flatten.map {|m| m[:idM]}.uniq
+      region_codes = Decidim::Organization::INITIATIVES_SETTINGS_ALLOWED_REGIONS.slice(*allowed_regions).values.pluck(:municipalities).flatten.map { |m| m[:idM] }.uniq
       return true if region_codes.blank?
 
       authorization = Decidim::Initiatives::UserAuthorizations.for(user).where(name: :csam).first
@@ -75,6 +75,25 @@ module OrganizationExtend
 
     def basic_auth_enabled?
       self.basic_auth_username.present? || self.basic_auth_password.present?
+    end
+
+    def omniauth_provider_settings(provider)
+      Rails.logger.info " TCO check 1 with provider #{provider}"
+      Rails.logger.info " TCO omniauth_provider_settings.size #{@omniauth_provider_settings.size}" if @omniauth_provider_settings
+      @omniauth_provider_settings ||= Hash.new do |hash, provider_key|
+        Rails.logger.info " TCO cache empty"
+        hash[provider_key] = begin
+          omniauth_settings.each_with_object({}) do |(key, value), provider_settings|
+            next unless key.to_s.include?(provider_key.to_s)
+            value = Decidim::AttributeEncryptor.decrypt(value) if Decidim::OmniauthProvider.value_defined?(value)
+            setting_key = Decidim::OmniauthProvider.extract_setting_key(key, provider_key)
+            Rails.logger.info " TCO decryption done for key #{setting_key}"
+            provider_settings[setting_key] = value
+          end
+        end
+      end
+      Rails.logger.info " TCO returns cached value for key #{provider}"
+      @omniauth_provider_settings[provider]
     end
 
   end
