31823a2ecc24c637d66161be1898a72d1afa11c9
Thomas Collignon <thomas.collignon@belighted.com>
2022-05-20 12:00:11 +0200

    remove me - debugging purpose

--- lib/extends/decidim-core/app/models/decidim/organization.rb
+++ lib/extends/decidim-core/app/models/decidim/organization.rb
@@ -77,12 +77,21 @@ module OrganizationExtend
       self.basic_auth_username.present? || self.basic_auth_password.present?
     end
 
+    # def omniauth_provider_settings(provider)
+    #   var = Decidim::Organization.omniauth_provider_settings(provider)
+    #   Rails.logger.info " TCO var: #{var}"
+    #   var
+    # end
+
     def omniauth_provider_settings(provider)
-      Rails.logger.info ' TCO check 1'
+      Rails.logger.info " TCO check 1 with provider #{provider}"
+      Rails.logger.info " TCO omniauth_provider_settings.size #{@omniauth_provider_settings.size}" if @omniauth_provider_settings
       @omniauth_provider_settings ||= Hash.new do |hash, provider_key|
+        Rails.logger.info " TCO cache empty"
         hash[provider_key] = begin
           omniauth_settings.each_with_object({}) do |(key, value), provider_settings|
             next unless key.to_s.include?(provider_key.to_s)
+            Rails.logger.info " TCO decryption"
             value = Decidim::AttributeEncryptor.decrypt(value) if Decidim::OmniauthProvider.value_defined?(value)
             setting_key = Decidim::OmniauthProvider.extract_setting_key(key, provider_key)
             provider_settings[setting_key] = value
@@ -90,6 +99,7 @@ module OrganizationExtend
         end
       end
       @omniauth_provider_settings[provider]
+      Rails.logger.info " TCO returns cached value #{@omniauth_provider_settings[provider]} for key #{provider}"
     end
 
   end
