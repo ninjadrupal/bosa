a0961bb972d1adb5ad2ce3cd5bb679a3ff78a707
Sergei Krylov <sergei.krylov@yahoo.com>
2022-07-25 11:28:17 +0300

    BOSA21Q1-853 Add initiatives into open data export.
    Initially the open data exporter supported only Component type data to be included in open data export (for example Meetings, Comments, Proposals, etc).
    The v0.24 introduced the first participatory space to be included into open data - Votings, the PR could be found here: https://github.com/decidim/decidim/pull/7388
    After, there was additional enhancement in open data exporter to improve RAM usage and performance: https://github.com/decidim/decidim/pull/8593
    
    This PR brings both open data exporter improvements to v0.22. In our case the Initiatives is the first participatory space that is included in open data.
    The `initiatives/participatory_space.rb` contains "tricks" to updates the existing configuration (registration) of the participatory space in Decidim, because it has already been initialized earlier in the process of gem initialization.

--- lib/extends/decidim-initiatives/lib/decidim/initiatives/participatory_space.rb
+++ lib/extends/decidim-initiatives/lib/decidim/initiatives/participatory_space.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
 
-# Initial :initiatives participatory space registration is already done on 'decidim-initiatives' gem initialization
-#
+# Note: Initial :initiatives participatory space registration is already done on 'decidim-initiatives' gem initialization
+
+
 # Define additional resource permissions for initiatives
 manifest = Decidim.find_resource_manifest(:initiatives_type)
 if manifest.present?
   manifest.actions = %w(create vote view_author_metadata)
 end
 
-#
-# Decidim.register_participatory_space(:initiatives) do |participatory_space|
-#   ...
-#   participatory_space.data_portable_entities = [
-#     "Decidim::Initiative",
-#     "Decidim::InitiativesVote"
-#   ]
-#   ...
-# end
+
+# Update the `.exports` section for :initiatives participatory space to include initiatives into open data
+manifest = Decidim.find_participatory_space_manifest(:initiatives)
+if manifest.present? && manifest.export_manifests.present?
+  export_manifest = manifest.export_manifests.find {|m| m.name == :initiatives}
+
+  if export_manifest.present? && !export_manifest.include_in_open_data
+    export_manifest.collection do |initiative|
+      Decidim::Initiative.where(id: initiative.id)
+    end
+
+    export_manifest.include_in_open_data = true
+  end
+end
